38fd035fd4cd77fe68968ded8b4b004369de7d4d
Frank D Evans <frankdevans@yahoo.com>
2015-07-07 15:54:42 -0500

    update EDA comments

--- eda.R
+++ eda.R
@@ -28,41 +28,10 @@ sotu %>%
 
 
 
-# TODO: filter out addresses to joint sessions that are not SOTU
+
 sotu[1]
 str_to_lower(string = sotu[1])
 str_detect(string = str_to_lower(string = sotu[1]),
            patter = 'state')
 str_detect(string = str_to_lower(string = sotu[1]),
            patter = 'union')  # joint, session
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# TODO: pull dates from text, str_extract
-
-
-
-# TODO: pull president name from text, file name
-# TODO: Extra Data - party of president
-# TODO: Extra Data - dates of war, indicator of war time.
-# TODO: [EDA] Word cloud of freqeunt terms, infrequent, # speeches included
